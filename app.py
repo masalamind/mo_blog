2e2fbecb731145979887ff1d57a1ecd20e6353ac
masalamind <themasalacollective@gmail.com>
2022-05-14 15:41:27 +0300

    Import dependencies for form, form validation, password securing

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask import flash, redirect, session, logging
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 from data import Articles
 
 app = Flask(__name__)
@@ -15,9 +19,14 @@ def index():
 def about():
   return render_template('about.html')
 
-@app.route('/articles')
+    
+@app.route('/articles/')
 def articles():
   return render_template('articles.html', articles=Articles)
+
+@app.route('/articles/article/<string:id>/')
+def article(id):
+  return render_template('article.html', id=id)
 
 if __name__ == '__main__':
   app.run(debug=True)
