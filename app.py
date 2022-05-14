800177a2d58d70be6f0782398b1b85a516b9741d
masalamind <themasalacollective@gmail.com>
2022-05-14 15:53:07 +0300

    Added a register route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask import flash, redirect, session, logging
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
@@ -28,5 +28,26 @@ def articles():
 def article(id):
   return render_template('article.html', id=id)
 
+
+class RegisterForm(Form):
+  name = StringField('Name', [validators.Length(min=1, max=50)])
+  username = StringField('Username', [validators.length(min=4, max=25)])
+  email = StringField('Email', [validators.Length(min=6, max=50)])
+  password = PasswordField('Password', [
+    validators.DataRequired(),
+    validators.EqualTo('confirm', message='Passwords do not match')
+  ])
+  
+  confirm = PasswordField('Confirm Password')
+  
+@app.route('/register', methods=['GET','POST']) 
+def register():
+  form = RegisterForm(request.form)
+  
+  if request.method == 'POST' and form.validate():
+    return render_template('register.html') 
+  return render_template('register.html', form=form) 
+  
+  
 if __name__ == '__main__':
   app.run(debug=True)
